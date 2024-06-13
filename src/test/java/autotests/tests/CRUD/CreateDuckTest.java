344b4422f1989dc23417ecab1aeb4a44fb1a381c
MariaKuznetsova <kudinova_ms@mail.ru>
2024-06-13 21:50:33 +0700

    param. create

--- src/test/java/autotests/tests/CRUD/CreateDuckTest.java
+++ src/test/java/autotests/tests/CRUD/CreateDuckTest.java
@@ -6,9 +6,11 @@ import com.consol.citrus.TestCaseRunner;
 import com.consol.citrus.annotations.CitrusResource;
 import com.consol.citrus.annotations.CitrusTest;
 import com.consol.citrus.context.TestContext;
+import com.consol.citrus.testng.CitrusParameters;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import org.springframework.http.HttpStatus;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Test;
 
@@ -72,5 +74,37 @@ public class CreateDuckTest extends DuckCRUDClient {
 
         // + Проверить что утка есть в БД, все параметры совпадают
         checkDuckPropertiesInDB(runner, duck);
+    }
+
+    @Test(dataProvider = "duckList")
+    @CitrusTest
+    @CitrusParameters({"payload", "runner", "context"})
+    public void successfulDuckCreate(Duck payload, @Optional @CitrusResource TestCaseRunner runner,
+                                     @Optional @CitrusResource TestContext context) {
+        runner.$(doFinally().actions(ctx ->
+                databaseUpdate(runner, "DELETE FROM DUCK WHERE ID=${duckId}")));
+
+        createDuckFromObject(runner, payload);
+        validateStatusBodyAndSaveIdByJsonPath(runner, HttpStatus.OK, jsonPath()
+                .expression("$.color", payload.color())
+                .expression("$.height", payload.height())
+                .expression("$.material", payload.material())
+                .expression("$.sound", payload.sound())
+                .expression("$.wingsState", payload.wingsState()));
+        payload.id(Integer.parseInt(context.getVariables().get("duckId").toString()));
+
+        // + Проверить что утка есть в БД, все параметры совпадают
+        checkDuckPropertiesInDB(runner, payload);
+    }
+
+    @DataProvider(name = "duckList")
+    public Object[][] DuckProvider() {
+        return new Object[][]{
+                {new Duck().color("green").height(0.15).material("rubber").sound("quack").wingsState("ACTIVE"), null, null},
+                {new Duck().color("green").height(1).material("rubber").sound("quack").wingsState("ACTIVE"), null, null},
+                {new Duck().color("green").height(0.15).material("glass").sound("quack").wingsState("ACTIVE"), null, null},
+                {new Duck().color("green").height(0.15).material("rubber").sound("ku").wingsState("ACTIVE"), null, null},
+                {new Duck().color("green").height(0.15).material("rubber").sound("quack").wingsState("FIXED"), null, null}
+        };
     }
 }
